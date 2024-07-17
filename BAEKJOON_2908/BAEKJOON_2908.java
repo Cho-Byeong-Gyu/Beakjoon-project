f1d84b8078cf36fc4ba181c28c2017012b6e468d
cho-byeong-gyu <chobyeonggyu03@gmail.com>
2024-07-18 02:21:27 +0900

    2908번 문제 풀이

---
+++ BAEKJOON_2908/BAEKJOON_2908.java
@@ -0,0 +1,39 @@
+package BAEKJOON_2908;
+
+/*
+ 날짜: 2024.07.17.수
+ 내용: 백준 2908번 (상수)
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BAEKJOON_2908 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		
+		String str = br.readLine();
+		
+		String[] num = str.split(" ");										// BufferedReader를 활용하기 위해 Split()메서드를 써서 숫자2개를 문자열로 입력받기
+	
+		int A = Integer.parseInt(new StringBuilder(num[0]).reverse().toString());		// StringBuilder와 reverse속성을 활용해 문자열 상태에서 숫자를 뒤집기
+		int B = Integer.parseInt(new StringBuilder(num[1]).reverse().toString());		// StringBuilder와 reverse속성을 활용해 문자열 상태에서 숫자를 뒤집기
+		
+		if(str == "") {									// 사용자가 문자열을 제대로 입력했는지 검증 - 아무것도 입력하지않으면 false의미의 0출력
+			System.out.println(0);
+			return;
+		}
+		
+		
+		if( A >= B ){
+			System.out.println(A);
+		} else {
+			System.out.println(B);
+		}
+		
+		
+		br.close();
+	}
+
+}
