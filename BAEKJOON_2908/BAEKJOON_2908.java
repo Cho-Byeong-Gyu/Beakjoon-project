dfade37df576509159fb2ce373458cd16c068acf
cho-byeong-gyu <chobyeonggyu03@gmail.com>
2024-07-18 02:28:55 +0900

    2908번 문제풀이

--- BAEKJOON_2908/BAEKJOON_2908.java
+++ BAEKJOON_2908/BAEKJOON_2908.java
@@ -15,25 +15,26 @@ public class BAEKJOON_2908 {
 		
 		String str = br.readLine();
 		
-		String[] num = str.split(" ");										// BufferedReader를 활용하기 위해 Split()메서드를 써서 숫자2개를 문자열로 입력받기
+		if (str.equals("")) {								// 사용자가 문자열을 제대로 입력했는지 검증하기
+			System.out.println("문자열을 입력하지 않으셨습니다.");				
+			return;
+		}
+		
+		String[] num = str.split(" ");									     	// BufferedReader를 활용하기 위해 Split()메서드를 써서 숫자2개를 문자열로 입력받기
 	
 		int A = Integer.parseInt(new StringBuilder(num[0]).reverse().toString());		// StringBuilder와 reverse속성을 활용해 문자열 상태에서 숫자를 뒤집기
 		int B = Integer.parseInt(new StringBuilder(num[1]).reverse().toString());		// StringBuilder와 reverse속성을 활용해 문자열 상태에서 숫자를 뒤집기
 		
-		if(str == "") {									// 사용자가 문자열을 제대로 입력했는지 검증 - 아무것도 입력하지않으면 false의미의 0출력
-			System.out.println(0);
-			return;
-		}
 		
 		
-		if( A >= B ){
+		if( A >= B ){															// 거꾸로 뒤집힌 숫자의 대소 비교후 더 큰 수를 출력
 			System.out.println(A);
 		} else {
 			System.out.println(B);
 		}
 		
 		
-		br.close();
+		br.close();																// Reader 닫아주기
 	}
 
 }
